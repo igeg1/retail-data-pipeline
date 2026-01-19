adad820f3332c6cb92ed48209938ef7b2c088ad4
Ivan Espinosa <ivangeg@icloud.com>
2026-01-19 14:12:09 -0600

    feat: added load function to save data to .csv files

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,4 @@
 from .etl import extract
+from .etl import load
 from .business import transform
 from .business import avg_weekly_sales_per_month
--- src/etl.py
+++ src/etl.py
@@ -8,7 +8,24 @@ def extract(store_data, extra_data, column = "index"):
     :param extra_data: File path to a .parquet file.
     :param column: Label (name) of the column to merge the two DataFrames on.
     """
+
     store_data_df = pd.read_csv(store_data)
     extra_df = pd.read_parquet(extra_data, engine="fastparquet")
     merged_df = store_data_df.merge(extra_df, on = column)
     return merged_df
+
+def load(full_data, full_data_file_path, agg_data, agg_data_file_path):
+    """
+    Loads two DataFrames into two separate .csv files using the specified paths.
+    
+    :param full_data: Pandas DataFrame with the full, clean data.
+    :param full_data_file_path: String with a file path to save full_data to.
+    :param agg_data: Pandas DataFrame with the aggregated sales data.
+    :param agg_data_file_path: String with a file path to save agg_datas to.
+    """
+
+    # Write the full data to a CSV
+    full_data.to_csv(full_data_file_path, index=False)
+
+    # Write the aggregated data to a CSV
+    agg_data.to_csv(agg_data_file_path, index=False)
