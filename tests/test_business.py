25bb90208eab71a903b5ec020624f52db6603347
Ivan Espinosa <ivangeg@icloud.com>
2026-01-20 09:20:59 -0600

    test: refactor unit tests to adhere to DRY principles using fixtures

--- tests/test_business.py
+++ tests/test_business.py
@@ -3,7 +3,7 @@ import pytest
 from src.business import transform
 from src.business import avg_weekly_sales_per_month
 
-@pytest.fixture()
+@pytest.fixture(scope="module")
 def raw_data():
     raw_data = pd.DataFrame({
         'Unnamed: 0': [0, 1, 2, 3, 4, 5, 6],
@@ -21,62 +21,55 @@ def raw_data():
     })
     return raw_data
 
-@pytest.fixture()
+@pytest.fixture(scope="module")
 def clean_data(raw_data):
     clean_data = transform(raw_data)
     return clean_data
 
+@pytest.fixture(scope="module")
+def agg_data(clean_data):
+    agg_data = avg_weekly_sales_per_month(clean_data)
+    return agg_data
 
-def test_transform_columns(raw_data):
-    result = transform(raw_data)
 
+def test_transform_columns(clean_data):
     expected_columns = ['Store_ID', 'Dept', 'IsHoliday', 'Weekly_Sales', 'CPI', 'Unemployment', 'Month']
 
-    assert result.columns.tolist() == expected_columns
+    assert clean_data.columns.tolist() == expected_columns
 
 
-def test_transform_imputes_missing_values(raw_data):
-    result = transform(raw_data)
-
+def test_transform_imputes_missing_values(clean_data):
     # Assert there are no NaN values in any column
-    assert result['Weekly_Sales'].isnull().sum() == 0
-    assert result['CPI'].isnull().sum() == 0
-    assert result['Unemployment'].isnull().sum() == 0
+    assert clean_data['Weekly_Sales'].isnull().sum() == 0
+    assert clean_data['CPI'].isnull().sum() == 0
+    assert clean_data['Unemployment'].isnull().sum() == 0
 
 
-def test_transform_ignores_smaller_sales(raw_data):
-    result = transform(raw_data)
-
+def test_transform_ignores_smaller_sales(clean_data):
     valid_sales = [46729.77, 21249.31, 11737.12, 19047.05, 10000.0]
 
     # Assert that only records w/ sales above 10_000 were kept
-    assert result['Weekly_Sales'].sum() == sum(valid_sales)
+    assert clean_data['Weekly_Sales'].sum() == sum(valid_sales)
 
 
-def test_transform_extracts_month(raw_data):
-    result = transform(raw_data)
-
+def test_transform_extracts_month(clean_data):
     expected_months = [2, 2, 3, 4, 3] 
 
     # Assert Months were successfully extracted from Date
-    assert result['Month'].tolist() == expected_months
+    assert clean_data['Month'].tolist() == expected_months
 
 
-def test_avg_sales_grouping_sorting(clean_data):
-    result = avg_weekly_sales_per_month(clean_data)
-
+def test_avg_sales_grouping_sorting(agg_data):
     expected_values = [2, 3, 4]
 
     # Asert the results were properly grouped into Months and sorted in order
-    assert result['Month'].to_list() == expected_values
+    assert agg_data['Month'].to_list() == expected_values
 
 
-def test_avg_sales_rounding(clean_data):
-    result = avg_weekly_sales_per_month(clean_data)
-
+def test_avg_sales_rounding(agg_data):
     avg_sale_m2 = round((46729.77 + 21249.31) / 2, 2)
     avg_sale_m3 = round((11737.12 + 10000.0) / 2, 2)
     expected_values = [avg_sale_m2, avg_sale_m3, 19047.05] 
 
     # Assert the results match the expected values
-    assert result['Weekly_Sales'].to_list() == expected_values
+    assert agg_data['Weekly_Sales'].to_list() == expected_values
